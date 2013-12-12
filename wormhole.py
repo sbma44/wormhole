a9a753e3fbdc1e53cecddc499abd4016c824e282
sbma44 <thomas.j.lee@gmail.com>
2013-12-12 12:08:38 -0500

    moving AWS region data to wormhole.py instead of db

--- wormhole.py
+++ wormhole.py
@@ -1,25 +1,57 @@
 import boto.ec2
 # import boto.manage.cmdshell
-import os, time
+import os, time, sqlite3
 from urllib import urlopen
 
-AMI_ID = 'ami-4836060d'
 AMI_USER_NAME = 'ec2-user'
 KEY_PAIR_NAME = 'wormhole-kp'
 SECURITY_GROUP_NAME = 'wormhole-sg'
 INSTANCE_SIZE = 't1.micro'
 KEY_PAIR_PATH = '.'
+SQLITE_DB = 'wormhole.db'
+
+def find_all_global_instances():
+	db = sqlite3.connect(SQLITE_DB)
+	cursor = db.cursor()
+	targets = []
+	for (name, region_id) in cursor.execute("SELECT name, id FROM regions"):
+		targets.append((name, region_id))
+	db.close()
+
+	for (name, region_id) in targets:
+		print region_id
+		wh = Wormhole(region_id)
+		for instance in wh.conn.get_only_instances():
+			wh.record_instance(instance)
 
 class Wormhole(object):
 	"""Creates EC2 instances for OpenVPN tunneling"""
 	def __init__(self, region='us-west-1'):
 		super(Wormhole, self).__init__()		
+
+		if not self.REGIONS.get(region):
+			raise Exception('Invalid AWS region.')
+
+		if len(self.REGIONS.get(region, {}).get('ami_id', ''))==0:
+			raise Exception('Wormhole AMI not yet available in region %s.' % region)
+
 		(aws_access_key, aws_secret_key) = self._get_credentials()
 		self.conn = boto.ec2.connect_to_region(region, aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)					
+		self.region = region
 		self.security_group = None
 		self.key_pair = None
 		self.public_ip = None
-		self.reservation = None
+		self.reservation = None		
+
+	def record_instance(self, instance):
+		print dir(instance)
+		db = sqlite3.connect(SQLITE_DB)
+		cursor = db.cursor()
+		print instance.id
+		cursor.execute("DELETE FROM instances WHERE instance_id=?", (instance.id,))
+		cursor.execute("INSERT INTO instances (instance_id, timestamp, region, ip, state, public_dns_name, instance_type, image_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?);", (instance.id, int(time.time()), self.region, instance.ip_address, instance.state, instance.public_dns_name, instance.instance_type, instance.image_id))
+		db.commit()
+		db.close()
 
 	def _get_credentials(self):
 		return (os.getenv('AWS_ACCESS_KEY'), os.getenv('AWS_SECRET_KEY'))
@@ -94,8 +126,33 @@ class Wormhole(object):
 		print 'Instance is running.'
 		self.instance_ip = instance.ip_address
 
+		
+
 	# def connect(self):
 	# 	self.cmdshell = boto.manage.cmdshell.sshclient_from_instance(instance, self._key_path(), user_name=AMI_USER_NAME)
+
+	def launch_tunnel_process(self):
+		# openvpn process call
+		openvpn_call = [
+			'openvpn'
+			'--client',
+			'--dev tun',
+			'--proto udp',
+			'--remote %s 1194' % self.instance_ip,
+			'--resolv-retry infinite',
+			'--nobind',
+			'--persist-key',
+			'--persist-tun',
+			'--ca ca.crt',
+			'--cert client.crt',
+			'--key client.key',
+			'--comp-lzo',
+			'--verb 3',
+			'--cd %s/openvpn' % os.getcwd()
+		]
+		self.tunnel_process = subprocess.Popen(openvpn_call, stdout=subprocess.PIPE)
+		return self.tunnel_process
+
 
 	def stop(self):
 		self.disable_access()
@@ -103,6 +160,84 @@ class Wormhole(object):
 			for instance in reservation.instances:
 				if instance.image_id==AMI_ID:
 					instance.terminate()
+
+	REGIONS = {
+		'us-east-1': {
+			u'ami_id': u'',
+	  		u'connection_type': u'HTTP and HTTPS',
+			u'domain': u'ec2.us-east-1.amazonaws.com',
+			u'lat': u'38.13',
+			u'lon': u'-78.45',
+			u'name': u'US East (Northern Virginia) Region',
+			u'short_name': u'Virginia'
+		},
+ 		'us-west-2': {
+	 		u'ami_id': u'',
+	  		u'connection_type': u'HTTP and HTTPS',
+	  		u'domain': u'ec2.us-west-2.amazonaws.com',
+			u'lat': u'46.15',
+			u'lon': u'-123.88',
+			u'name': u'US West (Oregon) Region',
+			u'short_name': u'Oregon'
+		},
+		'us-west-1': {
+			u'ami_id': u'ami-fc2616b9',
+			u'connection_type': u'HTTP and HTTPS',
+			u'domain': u'ec2.us-west-1.amazonaws.com',
+			u'lat': u'41.48',
+			u'lon': u'-120.53',
+			u'name': u'US West (Northern California) Region',
+			u'short_name': u'California'
+		},
+		'eu-west-1': {
+			u'ami_id': u'',
+			u'connection_type': u'HTTP and HTTPS',
+			u'domain': u'ec2.eu-west-1.amazonaws.com',
+			u'lat': u'53',
+			u'lon': u'-8',
+			u'name': u'EU (Ireland) Region',
+			u'short_name': u'Ireland'
+		},
+		'ap-southeast-1': {
+			u'ami_id': u'',
+			u'connection_type': u'HTTP and HTTPS',
+			u'domain': u'ec2.ap-southeast-1.amazonaws.com',
+			u'id': u'',
+			u'lat': u'1.37',
+			u'lon': u'103.8',
+			u'name': u'Asia Pacific (Singapore) Region',
+			u'short_name': u'Singapore'
+		},
+		'ap-southeast-2': {
+			u'ami_id': u'',
+			u'connection_type': u'HTTP and HTTPS',
+			u'domain': u'ec2.ap-southeast-2.amazonaws.com',	
+			u'lat': u'-33.86',
+			u'lon': u'151.2',
+			u'name': u'Asia Pacific (Sydney) Region',
+			u'short_name': u'Sydney'
+		},
+		'ap-northeast-1': {
+			u'ami_id': u'',
+			u'connection_type': u'HTTP and HTTPS',
+			u'domain': u'ec2.ap-northeast-1.amazonaws.com',
+			u'id': u'',
+			u'lat': u'35.41',
+			u'lon': u'139.42',
+			u'name': u'Asia Pacific (Tokyo) Region',
+			u'short_name': u'Tokyo'
+		},
+		'sa-east-1': {
+			u'ami_id': u'',
+			u'connection_type': u'HTTP and HTTPS',
+			u'domain': u'ec2.sa-east-1.amazonaws.com',
+			u'id': u'sa-east-1',
+			u'lat': u'-23.34',
+			u'lon': u'-46.38',
+			u'name': u'South America (Sao Paulo) Region',
+			u'short_name': u'Sao Paulo'
+		}
+	}
 
 wh = None
 
