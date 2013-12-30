57a86a0305d6353740f1a56c0192da2a3b2091f7
sbma44 <thomas.j.lee@gmail.com>
2013-12-30 18:12:18 -0500

    needs DHCP server

--- bootstrap/bootstrap-wormhole.sh
+++ bootstrap/bootstrap-wormhole.sh
@@ -4,7 +4,7 @@ NEW_HOSTNAME='wormhole'
 echo $NEW_HOSTNAME > /etc/hostname
 sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$NEW_HOSTNAME/g" /etc/hosts
 
-apt-get install -y memcached openvpn
+apt-get install -y memcached openvpn isc-dhcp-server
 pip install web.py boto python-memcached
 
 cp dhcpd.conf /etc/dhcp/
