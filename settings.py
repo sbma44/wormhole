90ee6bb48cdcd8b4cdc9b4a55f6374ea6566f3cf
Tom Lee <thomas.j.lee@gmail.com>
2014-03-03 16:18:33 -0500

    importing helpers in the correct spot

--- settings.py
+++ settings.py
@@ -1,10 +1,11 @@
 import os, json
-from wormhole_helpers import * 
 
 AWS_DIRECTORY = '/home/pi/Devel/wormhole/aws'
 AWS_CREDENTIALS_FILE = 'aws_credentials.json'
 REGION_FILE = 'aws_region.json'
 MEMCACHE_SERVER = '127.0.0.1:11211'
+
+from wormhole_helpers import * 
 
 try:
     from local_settings import *
